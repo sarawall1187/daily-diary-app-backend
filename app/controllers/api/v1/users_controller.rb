5dfb9869336ce77da8234e6a3be46489e5c4d9dd
sarawall1187 <sarawall1187@gmail.com>
2019-08-12 08:58:18 -0700

    added fastjson_api gem and created user serializer, updated users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,12 +5,16 @@ class Api::V1::UsersController < ApplicationController
   def index
     @users = User.all
 
-    render json: @users
+    # render json: @users
+    users_json = UserSerializer.new(@users).serialized_json
+    render json: users_json
   end
 
   # GET /users/1
   def show
-    render json: @user
+    # render json: @user
+    user_json = UserSerializer.new(@user).serialized_json
+    render json: user_json
   end
 
   # POST /users
