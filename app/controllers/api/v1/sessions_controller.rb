129731b8a58543feaf81f65963bd033c6e2199bf
sarawall1187 <sarawall1187@gmail.com>
2019-08-12 11:52:22 -0700

    created sessions controller and update routeS

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,8 @@
+class Api::V1::SessionsController < ApplicationController
+    def create
+    end
+
+    def signup
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   
+  get 'api/v1/login' to: 'sessions#create'
+
+
   namespace :api do
     namespace :v1 do
       resources :diaries
