d34c71666f147ed9d116edd8ea22d9dba809c078
sarawall1187 <sarawall1187@gmail.com>
2019-08-13 09:31:26 -0700

    added session, current_user, and cookies

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,9 +1,30 @@
 class Api::V1::SessionsController < ApplicationController
     def create
-        binding.pry
+       @user = User.find_by(username: params[:session][:username])
+
+       if @user && @user.authenticate(params[:session][:password])
+        session[:user_id] = @user.id
+            render json: @user
+       else
+        render json: {
+            error: "Invalid Input"
+        }
+       end
     end
 
     def signup
+    end
+
+
+
+    def get_current_user
+        if logged_in?
+            render json: current_user
+       else
+        render json: {
+            error: "Not Logged In"
+        }
+       end
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  
+  get 'api/v1/get_current_user' => 'api/v1/sessions#get_current_user'
   post 'api/v1/login' => 'api/v1/sessions#create'
 
 
