65dad08927184eba89dc4f7bfff73368ab8e502e
sarawall1187 <sarawall1187@gmail.com>
2019-08-15 09:43:14 -0700

    added index action to entries controller

--- app/controllers/api/v1/entries_controller.rb
+++ app/controllers/api/v1/entries_controller.rb
@@ -1,2 +1,16 @@
 class Api::V1::EntriesController < ApplicationController
+
+    def index
+        if logged_in?
+        @entries = current_user.entries
+
+        render json: {
+            @entries
+        }
+        else 
+            render json: {
+                error: "You must be logged in to see your diary entries."
+            }
+        end
+    end
 end
